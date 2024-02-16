787637000f8c3decc3bf02dfcc8154085ec50383
aryam <aali0365@stu.kau.edu.sa>
2024-02-16 16:33:12 +0300

    Aryam commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -7,6 +7,8 @@ public class JavaApplication1 {
     
     public static void main(String[] args) {
        System.out.println("hello githup");
+       System.out.println("hello , I am Aryam");
+       
     }
     
 }
